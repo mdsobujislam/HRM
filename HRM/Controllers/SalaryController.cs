579e6da9180019b3540fbafcf3cb46d0065707dc
mdsobujislam <sobujislam674@gmail.com>
2025-08-13 17:53:23 +0600

    Done

---
+++ HRM/Controllers/SalaryController.cs
@@ -0,0 +1,60 @@
+﻿using HRM.Interfaces;
+using HRM.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+
+namespace HRM.Controllers
+{
+    public class SalaryController : Controller
+    {
+        private readonly ISalaryService _salaryService;
+        private readonly IBranchService _branchService;
+        private readonly ISalaryHeadsService _salaryHeadsService;
+        public SalaryController(ISalaryService salaryService, IBranchService branchService, ISalaryHeadsService salaryHeadsService)
+        {
+            _salaryService = salaryService;
+            _branchService = branchService;
+            _salaryHeadsService = salaryHeadsService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var salaryHeadsList = await _salaryHeadsService.GetAllSalaryHeads();
+            ViewBag.SalaryHeadsList = salaryHeadsList.Select(s => new SelectListItem
+            {
+                Value = s.Id.ToString(),
+                Text = s.Salaryitems
+            }).ToList();
+            var branchList =await _branchService.GetAllBranch();
+            ViewBag.BranchList = branchList.Select(b => new SelectListItem
+            {
+                Value = b.Id.ToString(),
+                Text = b.Name
+            }).ToList();
+            var salaryList =await _salaryService.GetAllSalary();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveSalary(int[] SalaryHeadsId, int[] BranchId, int[] Sl, string[] Parameter, double[] Value)
+        {
+            for (int i = 0; i < SalaryHeadsId.Length; i++)
+            {
+                var salary = new Salary
+                {
+                    SalaryHeadsId = SalaryHeadsId[i],
+                    BranchId = BranchId[i],
+                    Sl = Sl.Length > i ? Sl[i] : 0,
+                    Parameter = Parameter.Length > i ? Parameter[i] : null,
+                    Value = Value[i]
+                };
+
+                await _salaryService.InsertSalary(salary);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
--- HRM/Services/SalaryService.cs
+++ HRM/Services/SalaryService.cs
@@ -41,6 +41,31 @@ namespace HRM.Services
             }
         }
 
+        //public async Task<List<Salary>> GetAllSalary()
+        //{
+        //    try
+        //    {
+        //        using (var connection = new SqlConnection(_connectionString))
+        //        {
+        //            connection.Open();
+        //            var subscriptionId = _baseService.GetSubscriptionId();
+        //            var userId = _baseService.GetUserId();
+        //            var branchId = await _baseService.GetBranchId(subscriptionId, userId);
+
+        //            var query = @"Select t1.Id,t1.MonthIndex,t1.Month,t1.Year,t1.FromDate,t1.ToDate,t1.GenDate,t1.EmployeeId,(select t2.Name from Branch t2 where t2.Id=t1.BranchId) as Branch,(Select t3.DesignationName from Designation t3 where t3.Id=t1.DesignationId) as Designation,(Select t4.EmployeeName from Employees t4 where t4.EmpId=t1.EmployeeId) as Employee,t1.Sl,t1.Parameter,t1.Value,t1.FinalAmount,t1.TxId,t1.RefTxId from Salary where t1.SubscriptionId=@subscriptionId ";
+
+        //            var result = await connection.QueryAsync<Salary>(query, new { subscriptionId });
+        //            return result.ToList();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw;
+        //    }
+        //}
+
+
+
         public async Task<List<Salary>> GetAllSalary()
         {
             try
@@ -52,7 +77,7 @@ namespace HRM.Services
                     var userId = _baseService.GetUserId();
                     var branchId = await _baseService.GetBranchId(subscriptionId, userId);
 
-                    var query = @"Select t1.Id,t1.MonthIndex,t1.Month,t1.Year,t1.FromDate,t1.ToDate,t1.GenDate,t1.EmployeeId,(select t2.Name from Branch t2 where t2.Id=t1.BranchId) as Branch,(Select t3.DesignationName from Designation t3 where t3.Id=t1.DesignationId) as Designation,(Select t4.EmployeeName from Employees t4 where t4.EmpId=t1.EmployeeId) as Employee,t1.Sl,t1.Parameter,t1.Value,t1.FinalAmount,t1.TxId,t1.RefTxId from Salary where t1.SubscriptionId=@subscriptionId ";
+                    var query = @"Select t1.id,t1.SalaryHeadsId,t1.BranchId,t1.Parameter,t1.Value from Salary t1 where t1.SubscriptionId=@subscriptionId ";
 
                     var result = await connection.QueryAsync<Salary>(query, new { subscriptionId });
                     return result.ToList();
@@ -63,6 +88,55 @@ namespace HRM.Services
                 throw;
             }
         }
+
+
+
+        //public async Task<bool> InsertSalary(Salary salary)
+        //{
+        //    try
+        //    {
+        //        using (var connection = new SqlConnection(_connectionString))
+        //        {
+        //            connection.Open();
+
+        //            var subscriptionId = _baseService.GetSubscriptionId();
+        //            var userId = _baseService.GetUserId();
+        //            var branchId = await _baseService.GetBranchId(subscriptionId, userId);
+        //            var companyId = await _baseService.GetCompanyId(subscriptionId);
+
+        //            var queryString = "insert into Salary (SalaryHeads,MonthIndex,Month,Year,FromDate,ToDate,GenDate,EmployeeId,DesignationId,DepartmentId,BranchId,Sl,Parameter,Value,FinalAmount,TxId,RefTxId,SubscriptionId,CompanyId,CreatedAt) values ";
+        //            queryString += "( @SalaryHeads@MonthIndex,@Month,@Year,@FromDate,@ToDate,@GenDate,@EmployeeId,@DesignationId,@DepartmentId,@BranchId,@Sl,@Parameter,@Value,@FinalAmount,@TxId,@RefTxId,@SubscriptionId,@CompanyId,@CreatedAt)";
+        //            var parameters = new DynamicParameters();
+        //            parameters.Add("SalaryHeads", salary.SalaryHeadsId, DbType.String);
+        //            parameters.Add("MonthIndex", salary.MonthIndex, DbType.String);
+        //            parameters.Add("Month", salary.Month, DbType.String);
+        //            parameters.Add("Year", salary.Year, DbType.String);
+        //            parameters.Add("FromDate", salary.FromDate, DbType.String);
+        //            parameters.Add("ToDate", salary.ToDate, DbType.String);
+        //            parameters.Add("GenDate", salary.GenDate, DbType.String);
+        //            parameters.Add("EmployeeId", salary.EmployeeId, DbType.String);
+        //            parameters.Add("GenDate", salary.GenDate, DbType.String);
+        //            parameters.Add("Sl", salary.Sl, DbType.String);
+        //            parameters.Add("Parameter", salary.Parameter, DbType.String);
+        //            parameters.Add("DesignationId", salary.DesignationId, DbType.Int64);
+        //            parameters.Add("DepartmentId", salary.DepartmentId, DbType.Int64);
+        //            parameters.Add("BranchId", salary.BranchId, DbType.Int64);
+        //            parameters.Add("SubscriptionId", subscriptionId);
+        //            parameters.Add("CompanyId", companyId);
+        //            parameters.Add("CreatedAt", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), DbType.String);
+        //            var success = await connection.ExecuteAsync(queryString, parameters);
+        //            if (success > 0)
+        //            {
+        //                return true;
+        //            }
+        //            return false;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw;
+        //    }
+        //}
 
         public async Task<bool> InsertSalary(Salary salary)
         {
@@ -77,16 +151,17 @@ namespace HRM.Services
                     var branchId = await _baseService.GetBranchId(subscriptionId, userId);
                     var companyId = await _baseService.GetCompanyId(subscriptionId);
 
-                    var queryString = "insert into OffDays (MonthIndex,Month,Year,FromDate,ToDate,GenDate,EmployeeId,DesignationId,DepartmentId,BranchId,Sl,Parameter,Value,FinalAmount,TxId,RefTxId,SubscriptionId,CompanyId,CreatedAt) values ";
-                    queryString += "( @MonthIndex,@Month,@Year,@FromDate,@ToDate,@GenDate,@EmployeeId,@DesignationId,@DepartmentId,@BranchId,@Sl,@Parameter,@Value,@FinalAmount,@TxId,@RefTxId,@SubscriptionId,@CompanyId,@CreatedAt)";
+                    var queryString = "insert into Salary (SalaryHeadsId,BranchId,Sl,Parameter,Value,SubscriptionId,CompanyId) values ";
+                    queryString += "( @SalaryHeadsId,@BranchId,@Sl,@Parameter,@Value,@SubscriptionId,@CompanyId)";
                     var parameters = new DynamicParameters();
-                    //parameters.Add("OffDay", salary.OffDay, DbType.String);
-                    //parameters.Add("DesignationId", salary.DesignationId, DbType.Int64);
-                    //parameters.Add("DepartmentId", salary.DepartmentId, DbType.Int64);
-                    //parameters.Add("BranchId", offDays.BranchId, DbType.Int64);
+                    parameters.Add("SalaryHeadsId", salary.SalaryHeadsId, DbType.Int64);
+                    parameters.Add("BranchId", salary.BranchId, DbType.Int64);
+                    parameters.Add("Sl", salary.Sl, DbType.Int64);
+                    parameters.Add("Parameter", salary.Parameter, DbType.Int64);
+                    parameters.Add("Value", salary.Value, DbType.Double);
                     parameters.Add("SubscriptionId", subscriptionId);
                     parameters.Add("CompanyId", companyId);
-                    parameters.Add("CreatedAt", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), DbType.String);
+                    //parameters.Add("CreatedAt", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), DbType.String);
                     var success = await connection.ExecuteAsync(queryString, parameters);
                     if (success > 0)
                     {
