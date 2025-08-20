44a4a1bff9c7e9ebdbbbe15549b7016af7d67655
mdsobujislam <sobujislam674@gmail.com>
2025-08-20 18:00:05 +0600

    Done

---
+++ HRM/Interfaces/ILoanApprovalService.cs
@@ -0,0 +1,12 @@
+﻿using HRM.Models;
+
+namespace HRM.Interfaces
+{
+    public interface ILoanApprovalService
+    {
+        Task<List<LoanApproval>> GetAllLoanApproval();
+        Task<bool> InsertLoanApproval(LoanApproval loanApproval);
+        Task<bool> UpdateLoanApproval(LoanApproval loanApproval);
+        Task<bool> DeleteLoanApproval(int id);
+    }
+}
